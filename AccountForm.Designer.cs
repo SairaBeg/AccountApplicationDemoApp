bf915db833761ce625a1030386b5028ef870f952
SairaBeg <sairabeg.dev@gmail.com>
2024-01-07 17:40:34 -0500

    Account create, set, get, clear

--- AccountForm.Designer.cs
+++ AccountForm.Designer.cs
@@ -28,31 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
+            txtId = new TextBox();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
+            txtName = new TextBox();
+            txtBalance = new TextBox();
+            btnCreate = new Button();
+            btnSet = new Button();
+            btnGet = new Button();
+            btnGC = new Button();
+            btnDestroy = new Button();
+            btnClear = new Button();
             SuspendLayout();
             // 
-            // textBox1
+            // txtId
             // 
-            textBox1.Location = new Point(144, 133);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(157, 23);
-            textBox1.TabIndex = 1;
+            txtId.Location = new Point(28, 35);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(157, 23);
+            txtId.TabIndex = 1;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(214, 115);
+            label1.Location = new Point(103, 17);
             label1.Name = "label1";
             label1.Size = new Size(17, 15);
             label1.TabIndex = 0;
@@ -62,7 +62,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(366, 115);
+            label2.Location = new Point(250, 17);
             label2.Name = "label2";
             label2.Size = new Size(39, 15);
             label2.TabIndex = 2;
@@ -71,97 +71,103 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(502, 115);
+            label3.Location = new Point(386, 17);
             label3.Name = "label3";
             label3.Size = new Size(48, 15);
             label3.TabIndex = 4;
             label3.Text = "Balance";
             // 
-            // textBox2
+            // txtName
             // 
-            textBox2.Location = new Point(306, 133);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(157, 23);
-            textBox2.TabIndex = 3;
+            txtName.Location = new Point(190, 35);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(157, 23);
+            txtName.TabIndex = 3;
             // 
-            // textBox3
+            // txtBalance
             // 
-            textBox3.Location = new Point(466, 133);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(157, 23);
-            textBox3.TabIndex = 5;
+            txtBalance.Location = new Point(350, 35);
+            txtBalance.Name = "txtBalance";
+            txtBalance.Size = new Size(157, 23);
+            txtBalance.TabIndex = 5;
             // 
-            // button1
+            // btnCreate
             // 
-            button1.Location = new Point(171, 176);
-            button1.Name = "button1";
-            button1.Size = new Size(132, 53);
-            button1.TabIndex = 6;
-            button1.Text = "Create";
-            button1.UseVisualStyleBackColor = true;
+            btnCreate.Location = new Point(55, 78);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(132, 53);
+            btnCreate.TabIndex = 6;
+            btnCreate.Text = "Create";
+            btnCreate.UseVisualStyleBackColor = true;
+            btnCreate.Click += btnCreate_Click;
             // 
-            // button2
+            // btnSet
             // 
-            button2.Location = new Point(323, 176);
-            button2.Name = "button2";
-            button2.Size = new Size(132, 53);
-            button2.TabIndex = 7;
-            button2.Text = "Set";
-            button2.UseVisualStyleBackColor = true;
+            btnSet.Location = new Point(207, 78);
+            btnSet.Name = "btnSet";
+            btnSet.Size = new Size(132, 53);
+            btnSet.TabIndex = 7;
+            btnSet.Text = "Set";
+            btnSet.UseVisualStyleBackColor = true;
+            btnSet.Click += btnSet_Click;
             // 
-            // button3
+            // btnGet
             // 
-            button3.Location = new Point(478, 176);
-            button3.Name = "button3";
-            button3.Size = new Size(132, 53);
-            button3.TabIndex = 8;
-            button3.Text = "Get";
-            button3.UseVisualStyleBackColor = true;
+            btnGet.Location = new Point(362, 78);
+            btnGet.Name = "btnGet";
+            btnGet.Size = new Size(132, 53);
+            btnGet.TabIndex = 8;
+            btnGet.Text = "Get";
+            btnGet.UseVisualStyleBackColor = true;
+            btnGet.Click += btnGet_Click;
             // 
-            // button4
+            // btnGC
             // 
-            button4.Location = new Point(478, 250);
-            button4.Name = "button4";
-            button4.Size = new Size(132, 53);
-            button4.TabIndex = 11;
-            button4.Text = "GC";
-            button4.UseVisualStyleBackColor = true;
+            btnGC.Location = new Point(362, 152);
+            btnGC.Name = "btnGC";
+            btnGC.Size = new Size(132, 53);
+            btnGC.TabIndex = 11;
+            btnGC.Text = "GC";
+            btnGC.UseVisualStyleBackColor = true;
+            btnGC.Click += btnGC_Click;
             // 
-            // button5
+            // btnDestroy
             // 
-            button5.Location = new Point(323, 250);
-            button5.Name = "button5";
-            button5.Size = new Size(132, 53);
-            button5.TabIndex = 10;
-            button5.Text = "Destroy";
-            button5.UseVisualStyleBackColor = true;
+            btnDestroy.Location = new Point(207, 152);
+            btnDestroy.Name = "btnDestroy";
+            btnDestroy.Size = new Size(132, 53);
+            btnDestroy.TabIndex = 10;
+            btnDestroy.Text = "Destroy";
+            btnDestroy.UseVisualStyleBackColor = true;
+            btnDestroy.Click += btnDestroy_Click;
             // 
-            // button6
+            // btnClear
             // 
-            button6.Location = new Point(171, 250);
-            button6.Name = "button6";
-            button6.Size = new Size(132, 53);
-            button6.TabIndex = 9;
-            button6.Text = "Clear";
-            button6.UseVisualStyleBackColor = true;
+            btnClear.Location = new Point(55, 152);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(132, 53);
+            btnClear.TabIndex = 9;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
             // 
             // AccountForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button4);
-            Controls.Add(button5);
-            Controls.Add(button6);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
+            ClientSize = new Size(555, 274);
+            Controls.Add(btnGC);
+            Controls.Add(btnDestroy);
+            Controls.Add(btnClear);
+            Controls.Add(btnGet);
+            Controls.Add(btnSet);
+            Controls.Add(btnCreate);
+            Controls.Add(txtBalance);
+            Controls.Add(txtName);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(textBox1);
+            Controls.Add(txtId);
             Name = "AccountForm";
             Text = "Account Form";
             Load += AccountForm_Load;
@@ -171,17 +177,17 @@
 
         #endregion
 
-        private TextBox textBox1;
+        private TextBox txtId;
         private Label label1;
         private Label label2;
         private Label label3;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
+        private TextBox txtName;
+        private TextBox txtBalance;
+        private Button btnCreate;
+        private Button btnSet;
+        private Button btnGet;
+        private Button btnGC;
+        private Button btnDestroy;
+        private Button btnClear;
     }
 }
--- AccountForm.cs
+++ AccountForm.cs
@@ -6,13 +6,49 @@ namespace AccountApplicationDemoApp
         {
             InitializeComponent();
         }
-
+        Account a;
         private void AccountForm_Load(object sender, EventArgs e)
         {
 
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+         a = new Account();
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            a.Id = int.Parse(txtId.Text);
+            a.Name = txtName.Text;
+            a.Balance = decimal.Parse(txtBalance.Text);
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            txtId.Text = a.Id.ToString();
+            txtName.Text = a.Name;
+            txtBalance.Text = a.Balance.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtId.Clear();
+            txtName.Clear();
+            txtBalance.Clear();
+        }
+
+        private void btnDestroy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGC_Click(object sender, EventArgs e)
         {
 
         }
